e708a8090b4d9815565acb551ef6db5c182264df
brapcan <74416013+brapcan@users.noreply.github.com>
2022-05-09 10:53:13 +0200

    Working bare chat app Working bare chat app

---
+++ tests/test_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django_chat_app import admin as django_chat_app_admin
+
+from . import models
+
+# Create your admin here.
---
+++ tests/test_app/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from django_chat_app import models as django_chat_app_models
+
+# Create your models here.
